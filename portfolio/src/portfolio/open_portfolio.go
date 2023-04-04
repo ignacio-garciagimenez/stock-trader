ddc22225debf61fedf57961f0eef6b58152b706c
Ignacio Garcia Gimenez <ignaciogarciagimenez@gmail.com>
2023-04-04 19:46:54 -0300

    open portfolio endpoint

--- portfolio/src/portfolio/open_portfolio.go
+++ portfolio/src/portfolio/open_portfolio.go
@@ -1,15 +1,48 @@
 package portfolio
 
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Handler[K any, V any] interface {
+	Handle(K) (V, error)
+}
+
+type OpenPortfolioEndpoint struct {
+	handler Handler[OpenPortfolioCommand, PortfolioId]
+}
+
+func (e *OpenPortfolioEndpoint) Open(c echo.Context) error {
+	command := new(OpenPortfolioCommand)
+	if err := c.Bind(command); err != nil {
+		return err
+	}
+
+	portfolioId, err := e.handler.Handle(*command)
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusCreated, struct {
+		PortfolioId PortfolioId `json:"portfolio_id"`
+	}{
+		PortfolioId: portfolioId,
+	})
+}
+
 type OpenPortfolioHandler struct {
 	portfolioRepository PortfolioRepository
 }
 
 type OpenPortfolioCommand struct {
-	name string
+	Name string `json:"name"`
 }
 
-func (h *OpenPortfolioHandler) Open(command OpenPortfolioCommand) (PortfolioId, error) {
-	portfolio, err := OpenPortfolio(command.name)
+func (h *OpenPortfolioHandler) Handle(command OpenPortfolioCommand) (PortfolioId, error) {
+	portfolio, err := OpenPortfolio(command.Name)
 	if err != nil {
 		return "", err
 	}
--- portfolio/src/portfolio/open_portfolio_test.go
+++ portfolio/src/portfolio/open_portfolio_test.go
@@ -1,20 +1,26 @@
 package portfolio
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"stock-trader/portfolio-context/src/common"
+	"strings"
 	"testing"
 
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_OpenPortfolio(t *testing.T) {
+func Test_OpenPortfolioHandler(t *testing.T) {
 	t.Run("Open portfolio with empty name", func(t *testing.T) {
 		handler := &OpenPortfolioHandler{
 			portfolioRepository: &InMemoryPortfolioRepository{},
 		}
 
-		portfolioId, err := handler.Open(OpenPortfolioCommand{
-			name: "    ",
+		portfolioId, err := handler.Handle(OpenPortfolioCommand{
+			Name: "    ",
 		})
 
 		assert.Empty(t, portfolioId)
@@ -27,8 +33,8 @@ func Test_OpenPortfolio(t *testing.T) {
 			portfolioRepository: &InMemoryPortfolioRepository{},
 		}
 
-		portfolioId, err := handler.Open(OpenPortfolioCommand{
-			name: "  Name  ",
+		portfolioId, err := handler.Handle(OpenPortfolioCommand{
+			Name: "  Name  ",
 		})
 
 		assert.Empty(t, portfolioId)
@@ -41,8 +47,8 @@ func Test_OpenPortfolio(t *testing.T) {
 			portfolioRepository: &InMemoryPortfolioRepository{},
 		}
 
-		portfolioId, err := handler.Open(OpenPortfolioCommand{
-			name: "  Name that is longer than 30 characters  ",
+		portfolioId, err := handler.Handle(OpenPortfolioCommand{
+			Name: "  Name that is longer than 30 characters  ",
 		})
 
 		assert.Empty(t, portfolioId)
@@ -53,21 +59,53 @@ func Test_OpenPortfolio(t *testing.T) {
 	t.Run("Open portfolio successfully", func(t *testing.T) {
 		repo := &InMemoryPortfolioRepository{
 			InMemoryBaseRepository: common.NewInMemoryBaseRepository[PortfolioId, *Portfolio](),
-			nameIndex: map[string]*Portfolio{},
+			nameIndex:              map[string]*Portfolio{},
 		}
 		handler := &OpenPortfolioHandler{
 			portfolioRepository: repo,
 		}
 
-		portfolioId, err := handler.Open(OpenPortfolioCommand{
-			name: "  A portfolio name  ",
+		portfolioId, err := handler.Handle(OpenPortfolioCommand{
+			Name: "  A portfolio name  ",
 		})
 
 		assert.Nil(t, err)
 		assert.NotEmpty(t, portfolioId)
-		assert.NotNil(t, func () *Portfolio {
+		assert.NotNil(t, func() *Portfolio {
 			portfolio, _ := repo.FindById(portfolioId)
 			return portfolio
 		}())
 	})
+}
+
+func Test_OpenPortfolioEndpoint(t *testing.T) {
+	t.Run("Open Portfolio Successfully", func(t *testing.T) {
+		portfolioId := PortfolioId(uuid.NewString())
+		endpoint := &OpenPortfolioEndpoint{
+			handler: &StubHandler[OpenPortfolioCommand, PortfolioId]{
+				call: func(command OpenPortfolioCommand) (PortfolioId, error) {
+					return portfolioId, nil
+				},
+			},
+		}
+
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodPost, "/portfolios", strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "A Portfolio name")))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		if assert.NoError(t, endpoint.Open(c)) {
+			assert.Equal(t, http.StatusCreated, rec.Code)
+			assert.Equal(t, fmt.Sprintf(`{"portfolio_id":"%s"}`, portfolioId) + "\n", rec.Body.String())
+		}
+	})
+}
+
+type StubHandler[K any, V any] struct {
+	call func(K) (V, error)
+}
+
+func (s *StubHandler[K, V]) Handle(command K) (V, error) {
+	return s.call(command)
 }
